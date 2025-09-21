a25cd01d8363845342c187a0ad939b8a53bc647c
Mukize <patrickmukize@gmail.com>
2025-09-21 18:50:16 +0200

    feat(frontend): map page

--- frontend/app/(tabs)/map.tsx
+++ frontend/app/(tabs)/map.tsx
@@ -1,11 +1,38 @@
-import MainPageBackground from "@/components/MainPageBackground";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Map from "@/components/Map";
+import { YStack, View, H4, Separator, Button } from "tamagui";
 
 export default function MapPage() {
   return (
-    <MainPageBackground>
-      <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      </SafeAreaView>
-    </MainPageBackground >
+    <SafeAreaView style={{ display: "flex", height: "100%" }} edges={["top", "bottom"]}>
+      <YStack
+        backgroundColor="white"
+        padding="$4"
+        gap="$3"
+        borderBottomWidth={1}
+        borderBottomColor="gray"
+      >
+        <H4>
+          (Destination)
+        </H4>
+        <Separator borderColor="rgba(128,0,128,0.2)" />
+        <Button borderRadius="$12" theme="blue">
+          Share my live location
+        </Button>
+      </YStack>
+      <View height="75%">
+        <Map />
+      </View>
+      <YStack
+        padding="$4"
+        backgroundColor="white"
+        borderTopWidth={1}
+        borderTopColor="gray"
+      >
+        <Button theme="green">
+          Drive completed
+        </Button>
+      </YStack>
+    </SafeAreaView>
   );
 }
