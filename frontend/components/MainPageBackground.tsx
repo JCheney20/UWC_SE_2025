f380d877bc1414dd3b3ec238ce8b4b37fcd522ab
Mukize <patrickmukize@gmail.com>
2025-09-21 12:16:40 +0200

    style(frontend): indenting

--- frontend/components/MainPageBackground.tsx
+++ frontend/components/MainPageBackground.tsx
@@ -1,12 +1,12 @@
 import { ImageBackground } from "expo-image";
 
 export default function MainPageBackground({ children }: { children: React.ReactNode }) {
-	return (
-		<ImageBackground
-			source={require('../assets/images/background-gradient.jpg')}
-			style={{ width: "100%", height: "100%" }}
-		>
-			{children}
-		</ImageBackground>
-	);
+  return (
+    <ImageBackground
+      source={require('../assets/images/background-gradient.jpg')}
+      style={{ width: "100%", height: "100%" }}
+    >
+      {children}
+    </ImageBackground>
+  );
 }
