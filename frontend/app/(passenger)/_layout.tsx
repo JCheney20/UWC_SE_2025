d7ceaa271fbef67f2c1a46ee30cae66085fac8da
Mukize <patrickmukize@gmail.com>
2025-09-28 15:36:35 +0200

    refactor(frontend): rename passenger layout function

--- frontend/app/(passenger)/_layout.tsx
+++ frontend/app/(passenger)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 
-export default function DriverLayout() {
+export default function PassengerLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" />
