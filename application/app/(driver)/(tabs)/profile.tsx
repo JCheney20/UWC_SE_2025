18a537832839633cde6165ef4415da9b4e16837e
Mukize <patrickmukize@gmail.com>
2025-10-10 18:53:50 +0200

    feat: add sign out button to driver profile

--- application/app/(driver)/(tabs)/profile.tsx
+++ application/app/(driver)/(tabs)/profile.tsx
@@ -1,10 +1,17 @@
-import GradientBackground from "@/components/GradientBackground";
 import { SafeAreaView } from "react-native-safe-area-context";
+import GradientBackground from "@/components/GradientBackground";
+import { Button } from "tamagui";
+import { supabase } from "@/utils/supabase";
+import { useMutation } from "@tanstack/react-query";
 
 export default function ProfilePage() {
+  const { mutate, isPending } = useMutation({
+    mutationFn: async () => await supabase.auth.signOut()
+  })
   return (
     <GradientBackground>
       <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Button onPress={() => mutate()}>{isPending ? "Signing out..." : "Sign out"}</Button>
       </SafeAreaView>
     </GradientBackground >
   );
